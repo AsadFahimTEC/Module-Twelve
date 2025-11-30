91af094e298a84fa877f9c17757d6de4dc780ed2
AsadFahimTEC <fah485434@gmail.com>
2025-11-30 11:07:14 +0600

    not found route and middleware added properly

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
@@ -44,12 +44,18 @@ const initDB = async () => {
 
 initDB();
 
+// logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+    next();
+}
+
 // parser
 app.use(express.json());
 // app.use(express.urlencoded()); // for form data
 
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
     res.send('Hello Next Level Developers!')
 })
 
@@ -223,7 +229,13 @@ app.get("/todos", async (req: Request, res: Response) => {
 });
 
 
-app.use()
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+        path: req.path,
+    })
+})
 
 
 app.listen(port, () => {
